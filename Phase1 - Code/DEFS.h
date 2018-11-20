9c6fbe5f8474f786ad5e50dbdc14fc8d1242f389
A00x40 <44728333+A00x40@users.noreply.github.com>
2018-11-20 17:40:21 +0200

    Update DEFS.h

--- Phase1 - Code/DEFS.h
+++ Phase1 - Code/DEFS.h
@@ -6,6 +6,7 @@
 //This file contains some global constants and definitions to be used in the project.
 enum ActionType //The actions supported (you can add more if needed)
 {
+	Select,
 	DRAW_LINE,		//Draw Line
 	DRAW_RECT,		//Draw Rectangle
 	DRAW_TRI,		//Draw Triangle
@@ -50,8 +51,9 @@ struct GfxInfo	//Graphical info of each figure (you may add more members)
 	color DrawClr;	//Draw color of the figure
 	color FillClr;	//Fill color of the figure
 	bool isFilled;	//Figure Filled or not
-	int BorderWdth;	//Width of figure borders
 	bool isInverted;
+	int BorderWdth;	//Width of figure borders
+	
 };
 
 #endif
